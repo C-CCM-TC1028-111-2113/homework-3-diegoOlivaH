e81352deed10ae729cf39596846a1627bf04e91c
diegoOlivaH <88850984+diegoOlivaH@users.noreply.github.com>
2021-08-29 19:52:08 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,7 +1,19 @@
-
+def año_bisiesto(año):
+    año==año
+    if (año%4==0 and año%100==0 and año%400==0):
+        print("True,",año,"is a leap-year")
+    elif (año%4==0 and año%100==0):
+        print("False,",año,"isn't a leap-year")
+    elif (año%4==0):
+        print("True,",año,"is a leap year")
+    else:
+        print("False,",año,"isn't a leap year")
 def main():
     #escribe tu código abajo de esta línea
-    pass
+y= int(input("Input the year"))
+
+año_bisiesto(y)
+
 
 if __name__=='__main__':
     main()
