7f97e26167441b55d44e3d213132ff9e77734750
diegoOlivaH <88850984+diegoOlivaH@users.noreply.github.com>
2021-08-29 19:19:37 -0500

    Update exercise.py

--- assignments/08TarjeteriaEspanola/src/exercise.py
+++ assignments/08TarjeteriaEspanola/src/exercise.py
@@ -1,7 +1,18 @@
-
+ef número_de_pliegos(pliegos,plumones):
+    pliegos*=12
+    plumones*=35
+    if (plumones<=pliegos):
+        return plumones
+    if(plumones>=pliegos):
+        return pliegos
 def main():
     #escribe tu código abajo de esta línea
-    pass
+pli= int(input("Input the number of sheets of albanene paper: "))
+plu= int(input("Input the number of markers: "))
+
+print("The maximum number of cards that can be done are:",número_de_pliegos(pli,plu))
+
+
 
 if __name__=='__main__':
     main()
