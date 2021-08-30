b848391d56c9f01f10e5fbda2994ac96e5d31fb0
diegoOlivaH <88850984+diegoOlivaH@users.noreply.github.com>
2021-08-29 20:22:26 -0500

    Update exercise.py

--- assignments/07VolumenPrismaRectangular/src/exercise.py
+++ assignments/07VolumenPrismaRectangular/src/exercise.py
@@ -1,7 +1,21 @@
+def area(base,depth):
+    base==base
+    depth==depth
+    AREA= base*depth
+    return AREA
 
+def volume(height):
+    area(b,d)
+    height==height
+    vol = area(b,d)*height
+    return float(vol)
 def main():
     #escribe tu código abajo de esta línea
-    pass
+b= float(input("Input the base: "))
+h= float(input("Input the height: "))
+d= float(input("Input the depth: "))
+
+print("The volume of your area is",volume(h))
 
 if __name__=='__main__':
     main()
